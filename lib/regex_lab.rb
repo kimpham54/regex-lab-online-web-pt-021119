4c3ed955f11fa2f06d2e27ec4019e07b354aa81e
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-03-31 06:33:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,12 +7,13 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/^(un).+?(ing)$/)
+text.scan(/\bun\w*ing\b/)
+
 
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b.{5}\b/)
+  text.scan(/\b\w{5}\b/)
 
 end
 
